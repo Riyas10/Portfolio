3432338161890d9f0425c30b0e5eaf9f9efa7380
Mohamed Riyas <47690769+Riyas10@users.noreply.github.com>
2023-08-21 15:57:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -25,34 +25,3 @@ darkmode.onclick = () => {
     document.body.classList.remove("active");
   }
 };
-  /* paste this line in verbatim */
-  window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
-  /* customize formbutton below*/     
-  formbutton("create", {
-    action: "https://formspree.io/f/xyyqwejq",
-    title: "How can we help?",
-    fields: [
-      { 
-        type: "email", 
-        label: "Email:", 
-        name: "email",
-        required: true,
-        placeholder: "your@email.com"
-      },
-      {
-        type: "textarea",
-        label: "Message:",
-        name: "message",
-        placeholder: "What's on your mind?",
-      },
-      { type: "submit" }      
-    ],
-    styles: {
-      title: {
-        backgroundColor: "#033a80"
-      },
-      button: {
-        backgroundColor: "#033a80"
-      }
-    }
-  });
