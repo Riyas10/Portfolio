33424e516ef7fcb7015ffbc4073865efa7058de2
Mohamed Riyas <47690769+Riyas10@users.noreply.github.com>
2023-09-28 17:34:00 +0400

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,12 @@ darkmode.onclick = () => {
     document.body.classList.remove("active");
   }
 };
+
+// thank you message
+const form = document.querySelector('form');
+const thankYouMessage = document.querySelector('#thank-you-message');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  thankYouMessage.classList.add('show');
+  setTimeout(() => form.submit(), 2000);
+});
